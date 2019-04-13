122b87b64d1e72afd927530193c2f71970247436
AndrewOverman <AndrewOverman0517@gmail.com>
2019-04-13 11:56:17 -0400

    Update setupMyFirebase.js

--- src/views/setupMyFirebase.js
+++ src/views/setupMyFirebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const config = {
-   apiKey: "AIzaSyDrRP1ewh9C5R2QhL4gvYz7pl_g6qBPL0k",
-   authDomain: "semesterproject-235b9.firebaseapp.com",
-   databaseURL: "https://semesterproject-235b9.firebaseio.com",
-   projectId: "semesterproject-235b9",
-   storageBucket: "semesterproject-235b9.appspot.com",
-   messagingSenderId: "358587526074"
+
 };
 firebase.initializeApp(config);
 
