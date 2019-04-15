cc31f48dacd49bd276cfb57d2b144efa11a88712
AndrewOverman <AndrewOverman0517@gmail.com>
2019-04-15 11:31:15 -0400

    Adding Routes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
 import Home from './views/Home.vue'
+import About  from './views/About.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/user-login',
       name: 'user-login',
       component: Login
+    },
+    {
+      path: '/About',
+      name: 'About',
+      component: About
     }
   ]
 })
