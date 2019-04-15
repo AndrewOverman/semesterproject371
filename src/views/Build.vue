f7594363b98abed3f24c502ec03cc6432112f9f2
AndrewOverman <AndrewOverman0517@gmail.com>
2019-04-15 15:05:23 -0400

    Add files via upload

---
+++ src/views/Build.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Your Champion is:</h1>
+        <img v-bind:src="'https://ddragon.leagueoflegends.com/cdn/9.7.1/img/champion/' + this.$props.champion + '.png'" alt="">
+    </div>
+</template>
+
+<style>
+    img{
+        text-align: center;
+        padding:3px
+    }
+</style>
+
+<script>
+    export default {
+        props: {
+            champion: {
+                type: Object
+            },
+        },
+        data() {
+            return {
+                userName: "Me"
+            }
+        },
+         methods: {
+             
+        }
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,15 @@
     <h1 class="title">Ultimate Bravery</h1>
     <button class="link" v-on:click="goToLogin">Login/Register</button>
     <button class="link" v-on:click="goToAbout">About</button>
+    <br><br>
+    <button class="link" v-on:click="reset">Reset list</button>
+    <button class="link" v-on:click="selectNone">Deselect None</button>
+    <button class="link" v-on:click="getChampion">Get Champion</button>
     <br>
     <br>
     <table>
       <template v-for="(champ,pos) in champs" class="champList">
-        <img v-bind:key="pos" v-bind:src="'https://ddragon.leagueoflegends.com/cdn/9.7.1/img/champion/' + champ + '.png'" alt="">
-  
+        <img v-bind:key="pos" v-on:click="greyOutChamp(pos,champ)" v-bind:id=pos v-bind:src="'https://ddragon.leagueoflegends.com/cdn/9.7.1/img/champion/' + champ + '.png'" alt="">
       </template>
       
     </table>
@@ -16,7 +19,6 @@
 </template>
 
 <script>
-  
   export default {
     props: {
 
@@ -24,6 +26,28 @@
     data() {
       return  {
         champs: ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", 
+          "Anivia", "Annie", "Ashe", "AurelionSol", "Azir", "Bard", 
+          "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", 
+          "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", 
+          "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", 
+          "Fizz", "Galio", "Gangplank", "Garen", "Gnar","Gragas", "Graves", 
+          "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", 
+          "JarvanIV", "Jax", "Jayce", "Jhin","Jinx", "Kaisa", "Kalista", 
+          "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", 
+          "Khazix", "Kindred", "Kled", "KogMaw", "Leblanc", "LeeSin", "Leona", 
+          "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", 
+          "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami", "Nasus", 
+          "Nautilus", "Neeko", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", 
+          "Ornn", "Pantheon", "Poppy", "Pyke", "Quinn", "Rakan", "Rammus", "RekSai", 
+          "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", 
+          "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", 
+          "Swain", "Sylas", "Syndra", "TahmKench", "Taliyah", "Talon", "Taric", 
+          "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", 
+          "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Velkoz", "Vi", 
+          "Viktor", "Vladimir", "Volibear", "Warwick", "MonkeyKing", "Xayah", "Xerath", 
+          "XinZhao", "Yasuo", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"
+        ],
+        owned: ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", 
           "Anivia", "Annie", "Ashe", "AurelionSol", "Azir", "Bard", 
           "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", 
           "Chogath", "Corki", "Darius", "Diana", "DrMundo", "Draven", 
@@ -59,6 +83,41 @@
       goToAbout(){
         this.$router.push({
           name: 'About'
+        });
+      },
+      greyOutChamp(pos,champ){
+        if(document.getElementById(pos).style.filter != "grayscale(100%)"){
+          document.getElementById(pos).style.filter = "grayscale(100%)";
+          this.$data.owned = this.$data.owned.filter(chmp=> chmp != champ);
+          console.log(this.$data.owned);
+        }
+        else{
+          document.getElementById(pos).style.filter = "grayscale(0%)";
+          this.$data.owned.push(champ);
+          this.$data.owned.sort();
+          console.log(this.$data.owned);
+        }
+      },
+      reset(){
+        for (var i = 0; i < this.$data.champs.length; i++) {
+          document.getElementById(i).style.filter = "grayscale(0%)";
+        } 
+        this.$data.owned = this.$data.champs;
+      },
+      selectNone(){
+        for (var i = 0; i < this.$data.champs.length; i++) {
+          document.getElementById(i).style.filter = "grayscale(100%)";
+        } 
+        this.$data.owned = [];
+        console.log(this.$data.owned);
+      },
+      getChampion(){
+        var champ = this.$data.owned[Math.floor(Math.random()*this.$data.owned.length)];
+        this.$router.push({
+          name: 'Build',
+          params: {
+            champion: champ
+          }
         });
       }
     }
